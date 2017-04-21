d1dd31f4dafad03dbd476e6b76ed93f3195240be
Jeremy Price <jfprice924@gmail.com>
2017-04-21 13:21:51 -0400

    Standardized variables and inserted comments

--- cite-keywords-mining.R
+++ cite-keywords-mining.R
@@ -1,3 +1,4 @@
+# Load in libraries
 library(readr)
 library(reshape2)
 library(widyr)
@@ -8,33 +9,22 @@ library(igraph)
 library(ggraph)
 library(tidytext)
 library(tm)
-#library(plyr)
 
-plot.words <- function(filename, plot.var, plot.threshold, plot.name){
-  set.seed(1234)
-  desc_cors %>%
-    filter(plot.var > plot.threshold) %>%
-    graph_from_data_frame() %>%
-    ggraph(layout = "fr") +
-    geom_edge_link(aes(edge_alpha = plot.var, edge_width = plot.var), edge_colour = "royalblue") +
-    geom_node_point(size = 5) +
-    geom_node_text(aes(label = plot.name), repel = TRUE,
-                   point.padding = unit(0.2, "lines")) +
-    theme_void()
-}
+# Load in raw data file from GitHub
+descriptors.frame <- read_csv("https://raw.githubusercontent.com/jeremyfprice/litreview/master/data/cite-keywords.csv", col_names = FALSE)
 
-# change TRUE to FALSE if you have no column headings in the CSV
-x <- read_csv("https://raw.githubusercontent.com/jeremyfprice/litreview/master/data/cite-keywords.csv", col_names = FALSE)
+# Convert dataframe from wide to long format (necessary for working with the data)
+descriptors.frame.long <- melt(descriptors.frame, id.vars = c("X1", "X2"))
 
-ggplot(data=x, aes(x[,2])) + geom_histogram()
+# Remove any empty cells
+descriptors.frame.long <- na.omit(descriptors.frame.long)
 
-x_long <- melt(x, id.vars = c("X1", "X2"))
+# Convert all data to lower case for easier manipulation
+descriptors.frame.long$value <- tolower(descriptors.frame.long$value)
 
-x_long <- na.omit(x_long)
-
-x_long$value <- tolower(x_long$value)
-
-x_long <- subset(x_long, !(value %in% c("preservice teachers", "technology integration", 
+# Define and remove stopwords, which will not be used in the analysis.
+# These words 
+descriptors.frame.long <- subset(descriptors.frame.long, !(value %in% c("preservice teachers", "technology integration", 
                                         "computer uses in education", 
                                         "preservice teacher education", 
                                         "technology uses in education", "teacher education", 
@@ -55,99 +45,96 @@ x_long <- subset(x_long, !(value %in% c("preservice teachers", "technology integ
                                         "effect size", "statistical significance", 
                                         "participant observation", "elementary secondary education")))
 
-x_long$value[grep("pedagogical content knowledge", x_long$value)] <- "tpack"
-x_long$value[grep("attitude", x_long$value)] <- "attitudes"
-x_long$value[grep("behavior", x_long$value)] <- "behavior"
-x_long$value[grep("games", x_long$value)] <- "games"
-x_long$value[grep("knowledge ", x_long$value)] <- "knowledge"
-x_long$value[grep("science instruction", x_long$value)] <- "stem education"
-x_long$value[grep("mathematics instruction", x_long$value)] <- "stem education"
-x_long$value[grep("lesson plans", x_long$value)] <- "planning_design"
-x_long$value[grep("instructional design", x_long$value)] <- "planning_design"
-x_long$value[grep("curriculum development", x_long$value)] <- "planning_design"
-x_long$value[grep("video technology", x_long$value)] <- "specific_technologies"
-x_long$value[grep("handheld devices", x_long$value)] <- "specific_technologies"
+# Combine similar descriptors
+descriptors.frame.long$value[grep("attitude", descriptors.frame.long$value)] <- "attitudes"
+descriptors.frame.long$value[grep("behavior", descriptors.frame.long$value)] <- "behavior"
+descriptors.frame.long$value[grep("games", descriptors.frame.long$value)] <- "games"
+descriptors.frame.ong$value[grep("knowledge ", descriptors.frame.long$value)] <- "knowledge"
+descriptors.frame.long$value[grep("science instruction", descriptors.frame.long$value)] <- "stem education"
+descriptors.frame.long$value[grep("mathematics instruction", descriptors.frame.long$value)] <- "stem education"
+descriptors.frame.long$value[grep("lesson plans", descriptors.frame.long$value)] <- "planning_design"
+descriptors.frame.long$value[grep("instructional design", descriptors.frame.long$value)] <- "planning_design"
+descriptors.frame.long$value[grep("curriculum development", descriptors.frame.long$value)] <- "planning_design"
+descriptors.frame.long$value[grep("video technology", descriptors.frame.long$value)] <- "specific_technologies"
+descriptors.frame.long$value[grep("handheld devices", descriptors.frame.long$value)] <- "specific_technologies"
 
+# Rename "pedagogical content knowledge" to "tpack"
+descriptors.frame.long$value[grep("pedagogical content knowledge", descriptors.frame.long$value)] <- "tpack"
+
+# Rejected changes
 #x_long$value[grep("teaching methods", x_long$value)] <- "methods"
 #x_long$value[grep("school teachers", x_long$value)] <- "elementary secondary teachers"
 #x_long$value[grep("science teachers", x_long$value)] <- "subject area teachers"
 #x_long$value[grep("english teachers", x_long$value)] <- "subject area teachers"
 #x_long$value[grep("mathematics teachers", x_long$value)] <- "subject area teachers"
 
-x_long <- x_long[ , !(names(x_long) %in% c("X2","variable"))]
-#duplicated.columns <- duplicated(t(x_long))
-#x_long.reduced <- x_long[, !duplicated.columns]
-x_long <- x_long[!duplicated(x_long), ]
+# Look for duplications and remove them
+descriptors.frame.long <- descriptors.frame.long[ , !(names(descriptors.frame.long) %in% c("X2","variable"))]
+descriptors.frame.long <- descriptors.frame.long[!duplicated(descriptors.frame.long), ]
 
-#View(x_long)
 
-x_long %>% 
+#dfl.count <- descriptors.frame.long %>% 
+#  group_by(value) %>% 
+#  count(sort = TRUE)
+
+# Conduct a count of descriptors
+descriptors.count <- descriptors.frame.long %>% 
   group_by(value) %>% 
   count(sort = TRUE)
 
-x_count <- x_long %>% 
-  group_by(value) %>% 
-  count(sort = TRUE)
+# Select only the descriptors that appear 4 or more times
+descriptors.count.sub <- subset(descriptors.count, n >= 4)
 
-head(x_count)
+descriptors.frame.long.sub <- filter(descriptors.frame.long, value %in% descriptors.count.sub$value)
 
-x_count.sub <- subset(x_count, n > (mean(x_count$n) + 1))
-#View(x_count.sub)
-
-x_long.sub <- filter(x_long, value %in% x_count.sub$value)
-
-desc_pairs <- x_long.sub %>% 
+# Determine descriptor pairs
+descriptors.pairs <- descriptors.frame.long.sub %>% 
   pairwise_count(value, X1, sort = TRUE, upper = FALSE)
 
-#View(desc_pairs)
-
-desc_cors <- x_long.sub %>% 
+# Conduct correlation analysis between descriptors
+descriptors.cors <- descriptors.frame.long.sub %>% 
   group_by(value) %>%
-  filter(n() >= 3) %>%
+  filter(n() >= 2) %>%
   pairwise_cor(value, X1, sort = TRUE, upper = FALSE)
 
-#View(desc_cors)
-
-
-desc_tf_idf <- x_long.sub %>% 
+# Conduct a tf_idf analysis
+descriptors.tf_idf <- descriptors.frame.long.sub %>% 
   count(X1, value, sort = TRUE) %>%
   ungroup() %>%
   bind_tf_idf(value, X1, n)
 
-desc_tf_idf %>% 
+descriptors.tf_idf %>% 
   arrange(tf_idf)
-#View(desc_tf_idf)
 
-# Just keep the important ones
-x_long.keep <- subset(x_long, value %in% x_long.sub$value)
+# Just keep the important descriptors
+descriptors.frame.long.keep <- subset(descriptors.frame.long, value %in% descriptors.frame.long.sub$value)
 
 # Replace spaces and remove other characters
-x_long.keep$value <- gsub(" ", "_", x_long.keep$value)
-x_long.keep$value <- gsub("[\\(\\)]", "", x_long.keep$value)
+descriptors.frame.long.keep$value <- gsub(" ", "_", descriptors.frame.long.keep$value)
+descriptors.frame.long.keep$value <- gsub("[\\(\\)]", "", descriptors.frame.long.keep$value)
 
-#View(x_long.keep)
+# Reshape table back into wide format
+descriptors.frame.wide.keep <- dcast(descriptors.frame.long.keep, formula = X1 ~ value)
 
-#x_wide.keep <- spread(x_long.keep, key = X1, value = value, fill = NA)
-x_wide.keep <- dcast(x_long.keep, formula = X1 ~ value)
-for(i in 1:ncol(x_wide.keep)){
-  x_wide.keep[is.na(x_wide.keep[,i]), i] <- (paste("no", colnames(x_wide.keep[i]), sep = "_"))
+# Rename column names to the descriptor that they hold
+for(i in 1:ncol(descriptors.frame.wide.keep)){
+  descriptors.frame.wide.keep[is.na(descriptors.frame.wide.keep[,i]), i] <- 
+    (paste("no", colnames(descriptors.frame.wide.keep[i]), sep = "_"))
 }
-colnames(x_wide.keep)[1] <- "manuscriptID"
 
-#View(x_wide.keep)
+# Rename first column to manuscriptID for standardization purposes
+colnames(descriptors.frame.wide.keep)[1] <- "manuscriptID"
 
-write.csv(x_wide.keep, file = "data/lit_review-keywords-processed.csv", row.names = FALSE)
+# Write table back into a csv file for further processing
+write.csv(descriptors.frame.wide.keep, file = "data/lit_review-keywords-processed.csv", 
+          row.names = FALSE)
 
-# rename NA from column name: http://stackoverflow.com/questions/38176326/replace-na-in-column-by-value-corresponding-to-column-name-in-seperate-table
+# Plot histogram based on year published
+ggplot(data=descriptors.frame, aes(descriptors.frame[,2])) + geom_histogram()
 
-#plot.words("", desc_cors$correlation, )
-
-#x_wide <- aggregate(value ~ X1, x_long.sub, paste)
-#x_wide.2 <- data.frame(matrix(unlist(x_wide$value)), stringsAsFactors = FALSE)
-#x_wide.3 <- data.frame(Reduce(rbind, x_wide$value))
-
+# Graph correlation relationships
 set.seed(1234)
-desc_cors %>%
+descriptors.cors %>%
   filter(correlation > .4) %>%
   graph_from_data_frame() %>%
   ggraph(layout = "fr") +
@@ -157,8 +144,9 @@ desc_cors %>%
                  point.padding = unit(0.2, "lines")) +
   theme_void()
 
+# Graph paired relationships
 set.seed(1234)
-desc_pairs %>%
+descriptors.pairs %>%
   filter(n >= 2) %>%
   graph_from_data_frame() %>%
   ggraph(layout = "fr") +
